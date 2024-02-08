edd8376383deba6eecae85d8d6470ed860ef8c9a
TheFloatingPixel <cube.kamil@gmail.com>
2024-02-09 00:56:20 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,96 @@
+wasm4keyboard = {
+    _enabled: false,
+    console: null,
+    notifications: null,
+    clock: false,
+    
+    initialize: function() {
+        this.console = document.getElementsByTagName("wasm4-app")[0];
+
+        if (console == null) {
+            console.error("[WASM-4 Keyboard] <wasm4-app> couldn't be found!");
+            return;
+        }
+        
+        this.notifications = this.console.shadowRoot.children[0].children[0];
+        
+        document.addEventListener('keydown', (e) => {     
+            if (e.keyCode == 13 && e.ctrlKey) {
+                wasm4keyboard.enabled = !wasm4keyboard.enabled;
+            }
+            
+            if ((!wasm4keyboard.enabled) || wasm4keyboard.console.showMenu) return;
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            e.stopPropagation();
+            
+            if (e.keyCode == 13 && e.altKey && !wasm4keyboard.console.showMenu)  {
+                    wasm4keyboard.console.showMenu = true;
+                    setTimeout(() => wasm4keyboard.console.showMenu = true, 10);
+            }
+            
+            wasm4keyboard.input(true, e);
+            
+            return false;
+        });
+        
+        document.addEventListener('keyup', (e) => {
+            if (!wasm4keyboard.enabled || wasm4keyboard.console.showMenu) return;
+            
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            wasm4keyboard.input(false, e);
+        })
+    },
+    
+    input: function(down, event) {
+        let isAscii = (event.key.length == 1) && (event.key.codePointAt() <= 255);
+    
+        if (event.keyCode == 0) return;
+    
+        this.console.inputState.gamepad[0] |= 8;
+        this.console.inputState.gamepad[1] = (
+            this.clock
+            | (down << 1)
+            | (event.shiftKey << 2)
+            | (event.ctrlKey << 3)
+            | (event.altKey << 4)
+            | (event.metaKey << 5)
+            | (event.getModifierState('CapsLock') << 6)
+            | (isAscii << 7)
+        );
+        this.console.inputState.gamepad[2] = isAscii? event.key.codePointAt(): 0;
+        this.console.inputState.gamepad[3] = event.keyCode;
+        this.clock = !this.clock;
+    },
+    
+    get enabled() {
+        return this._enabled;
+    },
+    
+    set enabled(value) {
+        this._enabled = value;
+        this.clock = false;
+        
+        if (value) {
+            this.console.inputState.gamepad[0] |= 8;
+            
+            this.notifications.show("Keyboard enabled!");
+            this.notifications.show("- Use ALT+ENTER to pause.");
+            this.notifications.show("- Use CTRL+ENTER to disable.")
+        } else {
+            this.console.inputState.gamepad[0] &= ~8;
+            
+            this.notifications.show("Keyboard disabled!");
+            this.notifications.show("- Use CTRL+ENTER to reenable.");
+        }
+    }
+}
+
+if (document.readyState != "complete") {
+    document.addEventListener('load', () => wasm4keyboard.initialize());
+} else {
+    wasm4keyboard.initialize();
+    wasm4keyboard.enabled = true;
+}
